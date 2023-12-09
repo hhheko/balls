6e1fd7c13c89adc0e0db17bf1b348425880be5a4
petr5092 <136697820+petr5092@users.noreply.github.com>
2023-12-09 13:29:45 +0300

    Delete n5.py

--- n5.py
+++
@@ -1,8 +0,0 @@
-data = [int(i) for i in open('17_5.txt').readlines()]
-answer_list = []
-for i in range(len(data) - 1):
-    if data[i] % 5 == 0 or data[i + 1] % 5 == 0:
-        if (data[i] + data[i + 1]) % 7 == 0:
-            answer_list.append(data[i] + data[i + 1])
-print(len(answer_list), max(answer_list))
-
